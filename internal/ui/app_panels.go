69197a500ea1af76aa665b5c3e754978586cee9f
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-02-28 19:00:22 +0100

    break app.go (#21)

---
+++ internal/ui/app_panels.go
@@ -0,0 +1,51 @@
+package ui
+
+func (m App) closeLogs() App {
+	if m.logsStop != nil {
+		m.logsStop()
+		m.logsStop = nil
+	}
+	m.logsVisible = false
+	m.logsLines = nil
+	m.logsContainer = ""
+	m.logsScrollOffset = 0
+	m.logsAutoScroll = true
+	m.table.SetHeight(m.tableHeight())
+	return m
+}
+
+func (m App) closeInspect() App {
+	m.inspectVisible = false
+	m.inspectLines = nil
+	m.inspectContainer = ""
+	m.inspectOffset = 0
+	m.table.SetHeight(m.tableHeight())
+	return m
+}
+
+func (m App) closeStats() App {
+	m.statsVisible = false
+	m.statsEntry = nil
+	m.statsContainer = ""
+	m.statsContainerID = ""
+	m.table.SetHeight(m.tableHeight())
+	return m
+}
+
+func (m App) tableHeight() int {
+	reserved := 8
+	if m.logsVisible {
+		reserved += logsPanelHeight
+	}
+	if m.inspectVisible {
+		reserved += inspectPanelHeight
+	}
+	if m.statsVisible {
+		reserved += statsPanelHeight
+	}
+	h := m.height - reserved
+	if h < 3 {
+		return 3
+	}
+	return h
+}
