8eb9eb2a472eb3e9ac264f97a5e80d64b58f4d49
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2026-03-04 22:14:30 +0100

    Add version flag support (#99)

--- main.go
+++ main.go
@@ -8,7 +8,14 @@ import (
 	"github.com/pivovarit/tdocker/internal/ui"
 )
 
+var version = "dev"
+
 func main() {
+	if len(os.Args) == 2 && (os.Args[1] == "-v" || os.Args[1] == "--version") {
+		fmt.Println("tdocker", version)
+		return
+	}
+
 	p := tea.NewProgram(ui.New())
 	if _, err := p.Run(); err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, "error:", err)
